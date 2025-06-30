98fee3891368ffd20bf47db0910a36845c6a3e3a
krotohmi <tri.pm.1405@gmail.com>
2025-07-01 03:50:17 +0700

    fix server domain

--- src/apis/app.api.ts
+++ src/apis/app.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import qs from 'qs';
 import KApi from '@krotohmi/k-api';
 
-const Domain = process.env.API_DOMAIN || 'https://k-system-server.onrender.com/';
+const Domain = process.env.API_DOMAIN || 'https://k-system-server.onrender.com';
 
 console.log('Domain', Domain);
 
