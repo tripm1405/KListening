c97c2aafe2ef4a216f02d6e5c26fc9d22c93f2f4
krotohmi <tri.pm.1405@gmail.com>
2025-07-01 07:58:27 +0700

    fix env import

--- src/apis/app.api.ts
+++ src/apis/app.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import qs from 'qs';
 import KApi from '@krotohmi/k-api';
 
-const Domain = process.env.API_DOMAIN || 'https://k-system-server.onrender.com';
+const Domain = process.env.NEXT_PUBLIC_API_DOMAIN || 'http://localhost:5000';
 
 console.log('Domain', Domain);
 
